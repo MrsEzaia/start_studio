021517287c854aed6a15a6437657276dc926b7a5
MrsEzaia <mrs.ezaia@mail.ru>
2022-12-09 12:56:58 +0300

    2022/12/09

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
+if (document.documentElement.clientWidth < 768) {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  spaceBetween: 16,
-  slidesPerView: "auto",
-  slidesOffsetAfter: 70,
-  centerSlides: true,
-});
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    spaceBetween: 16,
+    slidesPerView: "auto",
+    slidesOffsetAfter: 70,
+    centerSlides: true,
+  });
 
-const swiperTodo = new Swiper("#swiper-todo", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
+  const swiperTodo = new Swiper("#swiper-todo", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
 
-  spaceBetween: 16,
-  slidesPerView: "auto",
-  slidesOffsetAfter: 70,
-  centerSlides: true,
+    spaceBetween: 16,
+    slidesPerView: "auto",
+    slidesOffsetAfter: 70,
+    centerSlides: true,
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
